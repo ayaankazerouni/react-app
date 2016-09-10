ee91dd4d9aaa43f7f3920b53446a0089d9200f3b
Ayaan Kazerouni <ayaan@vt.edu>
2016-09-09 20:50:05 -0400

    Added some more seed data; added to llama enum.

--- models/vote.js
+++ models/vote.js
@@ -8,7 +8,7 @@ var Vote = new Schema({
   },
   vote: {
     type: String,
-    enum: [ 'sherriff_llama', 'deep_llama', 'weird_llama', 'cool_dude_llama', 'awkward_llama' ],
+    enum: [ 'sherriff_llama', 'deep_llama', 'weird_llama', 'hippie_llama', 'awkward_llama', 'surfer_dude_llama' ],
     required: true
   }
 });
