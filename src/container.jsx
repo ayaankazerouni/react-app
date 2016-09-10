86563904b1875189f1af85decea8848ce712fb5d
Ayaan Kazerouni <ayaan@vt.edu>
2016-09-09 21:50:41 -0400

    Dynamically adding llama radio buttons.

--- src/container.jsx
+++ src/container.jsx
@@ -9,32 +9,48 @@ var Container = React.createClass({
   getInitialState: function() {
     return { data: [] };
   },
-  loadVotes: function () {
+  loadVotes() {
     var url = '/votes';
     $.ajax({
       url: url,
       type: 'get',
       dataType: 'json',
       cache: false,
-      success: function (data) {
+      success: (data) => {
         this.setState({data: data});
-      }.bind(this),
-      error: function (xhr, status, err) {
+      },
+      error: (xhr, status, err) => {
         console.error(url, status, err.toString());
-      }.bind(this)
+      }
     });
   },
-  componentDidMount: function() {
-    this.loadVotes()
+  loadLlamas() {
+    var url = '/llamas';
+    $.ajax({
+      url: url,
+      type: 'get',
+      dataType: 'json',
+      cache: false,
+      success: (data) => {
+        this.setState({ llamas: data });
+      },
+      error: (xhr, status, err) => {
+        console.error(url, status, err.toString());
+      }
+    });
+  },
+  componentDidMount() {
+    this.loadLlamas();
+    this.loadVotes();
     pollInterval = setInterval(this.loadVotes, 2000);
   },
-  render: function() {
+  render() {
     return (
       <div className='col-12'>
         <h1>Bahama Llama 2017</h1>
         <ul className='header'>
           <li><IndexLink to='/' activeClassName='active'>Home</IndexLink></li>
-          <li><Link to='/llamas' activeClassName='active'>Llamas</Link></li>
+          <li><Link to={`/llamas/${this.state.llamas}`} activeClassName='active'>Llamas</Link></li>
           <li><Link to='/about' className='right' activeClassName='active'>About Me</Link></li>
         </ul>
         <div id='content'>
