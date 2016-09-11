c258c6ef2a424eebfba524cea1f9fc136862da2e
Ayaan Kazerouni <ayaan@vt.edu>
2016-09-11 00:38:52 -0400

    Vote schema has name field.

--- models/vote.js
+++ models/vote.js
@@ -2,13 +2,21 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Vote = new Schema({
+  name: String,
   email: {
     type: String,
     required: true
   },
   vote: {
     type: String,
-    enum: [ 'sherriff_llama', 'deep_llama', 'suspicious_llama', 'hippie_llama', 'awkward_llama', 'surfer_dude_llama' ],
+    enum: [
+      'sherriff_llama',
+      'deep_llama',
+      'suspicious_llama',
+      'fabulous_llama',
+      'awkward_llama',
+      'surfer_dude_llama'
+    ],
     required: true
   }
 });
