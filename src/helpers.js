dc2ec38ee9f0881ac33427471f3f107004029d5c
Ayaan Kazerouni <ayaan@vt.edu>
2016-09-11 02:58:36 -0400

    Styling to form

--- src/helpers.js
+++ src/helpers.js
@@ -10,44 +10,42 @@ var helpers = {
       show_label: true
     });
   },
-  initFormEventListeners() {
-    initFormInputListeners();
+  initFormEvents() {
+    $.support.placeholder = false;
+    test = document.createElement('input');
+    if('placeholder' in test) $.support.placeholder = true;
 
-    $('ul.thumbnails li').on('click', function() {
+    if ($.support.placeholder) {
+      $('form li.form-input').each(function() {
+        $(this).addClass('js-hide-label');
+      });
+    }
+
+    $('form li.form-input').find('input').on('keyup blur focus', function(e) {
+      var self = $(this);
+      var parent = self.parent();
+
+      if (e.type == 'keyup') {
+        parent.toggleClass('js-hide-label', self.val() === '');
+      } else if (e.type == 'blur') {
+        parent.toggleClass('js-hide-label', self.val() === '');
+        if (self.val() !== '') {
+          parent.addClass('js-unhighlight-label');
+        }
+      } else if (e.type == 'focus') {
+        if (self.val() !== '') {
+          parent.removeClass('js-unhighlight-label');
+        } else {
+          parent.toggleClass('js-hide-label');
+        }
+      }
+    });
+
+    $('ul.thumbnails li').on('click touchstart', function() {
       $('ul.thumbnails .thumbnail').removeClass('selected');
       $(this).find('div').addClass('selected');
-    });
+    })
   }
-};
-
-function initFormInputListeners() {
-  $.support.placeholder = false;
-  test = document.createElement('input');
-  if('placeholder' in test) $.support.placeholder = true;
-
-  if ($.support.placeholder) {
-    $('form li.input').each(function() {
-      $(this).addClass('js-hide-label');
-    });
-  }
-
-  $('form li.input').find('input').on('keyup blur focus', function(e) {
-    var self = $(this);
-    var parent = self.parent();
-
-    if (e.type == 'keyup') {
-      parent.toggleClass('js-hide-label', self.val() === '');
-    } else if (e.type == 'blur') {
-      parent.toggleClass('js-hide-label', self.val() === '');
-      if (self.val() !== '') {
-        parent.addClass('js-unhighlight-label');
-      }
-    } else if (e.type == 'focus') {
-      if (self.val() !== '') {
-        parent.removeClass('js-unhighlight-label');
-      }
-    }
-  });
 }
 
 module.exports = helpers;
