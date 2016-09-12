07aa11cee4ecca014c3c98fa3cd2d55e73c06485
Ayaan Kazerouni <ayaan@vt.edu>
2016-09-12 01:08:49 -0400

    Slow down result polling

--- src/container.jsx
+++ src/container.jsx
@@ -28,7 +28,7 @@ var Container = React.createClass({
   componentDidMount() {
     helpers.initSmoothScroll();
     this.loadVotes();
-    var pollInterval = setInterval(this.loadVotes, 2000);
+    var pollInterval = setInterval(this.loadVotes, 5000);
   },
   render() {
     var topLinkStyle = {textAlign: 'right'};
